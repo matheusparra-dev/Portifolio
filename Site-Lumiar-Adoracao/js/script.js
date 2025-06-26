3f8655c1dbb59e9594c9f05057645bea58045819
parramatheus <matheusparra@live.com>
2025-06-26 16:57:52 -0300

    Finalizado home mobile

--- Site-Lumiar-Adoracao/js/script.js
+++ Site-Lumiar-Adoracao/js/script.js
@@ -4,6 +4,19 @@ window.addEventListener("scroll", function() {
 })
 
 window.addEventListener("scroll", function() {
+    let nav = document.querySelector('.menu-mobile')
+    nav.classList.toggle('rolagem',window.scrollY > 0)
+})
+
+window.addEventListener("scroll", function() {
     let nav = document.querySelector('.scroll')
     nav.classList.toggle('rolagem',window.scrollY > 0)
 })
+
+function botaomenu() {
+    if (menuretratil.style.display == 'block') {
+        menuretratil.style.display = 'none'
+    } else {
+        menuretratil.style.display = 'block'
+    }
+}
