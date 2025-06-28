6ef5f6d0f96c1027687835a61e20be9fcbbbc972
parramatheus <matheusparra@live.com>
2025-06-28 01:49:16 -0300

    pequenos ajustes

--- Site-Lumiar-Adoracao/js/script.js
+++ Site-Lumiar-Adoracao/js/script.js
@@ -12,11 +12,35 @@ window.addEventListener("scroll", function() {
     let nav = document.querySelector('.scroll')
     nav.classList.toggle('rolagem',window.scrollY > 0)
 })
-
-function botaomenu() {
-    if (menuretratil.style.display == 'block') {
-        menuretratil.style.display = 'none'
-    } else {
-        menuretratil.style.display = 'block'
+    function botaomenu() {
+        recolher();
+        sebotaomenu();
     }
-}
+
+    function recolher() {
+        if (menuretratil.style.display == 'block') {
+            menuretratil.style.display = 'none'
+        } else {
+            menuretratil.style.display = 'block'
+        }
+    }
+
+    function sebotaomenu() {
+        const elemento = document.getElementById("icone-menu");
+        if (elemento == "menu") {
+            xbotaomenu();
+        } else {
+            mbotaomenu();
+        }
+    }
+
+
+    function xbotaomenu() {
+        const elemento2 = document.getElementById("icone-menu");
+        elemento2.innerHTML = "close";
+    }
+
+    function mbotaomenu() {
+        const elemento2 = document.getElementById("icone-menu");
+        elemento2.innerHTML = "menu";
+    }
